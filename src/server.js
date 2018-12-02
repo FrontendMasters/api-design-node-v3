9e17fdced351df65018bef1d7aeea6cb56aa21d0
Scott Moss <willscottmoss@gmail.com>
2018-12-01 22:51:30 -0800

    chore

--- src/server.js
+++ src/server.js
@@ -5,9 +5,6 @@ import config from './config'
 import cors from 'cors'
 import { signup, signin, protect } from './utils/auth'
 import { connect } from './utils/db'
-import userRouter from './resources/user/user.router'
-import itemRouter from './resources/item/item.router'
-import listRouter from './resources/list/list.router'
 
 export const app = express()
 
@@ -20,10 +17,6 @@ app.use(morgan('dev'))
 
 app.post('/signup', signup)
 app.post('/signin', signin)
-
-app.use('/api/user', userRouter)
-app.use('/api/item', itemRouter)
-app.use('/api/list', listRouter)
 
 export const start = async () => {
   try {
