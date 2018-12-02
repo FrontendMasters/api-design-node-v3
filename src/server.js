44fd033a04b660c355cbb66e1cb020d708b583e8
Scott Moss <willscottmoss@gmail.com>
2018-12-01 22:56:19 -0800

    chore

--- src/server.js
+++ src/server.js
@@ -3,11 +3,7 @@ import { json, urlencoded } from 'body-parser'
 import morgan from 'morgan'
 import config from './config'
 import cors from 'cors'
-import { signup, signin, protect } from './utils/auth'
 import { connect } from './utils/db'
-import userRouter from './resources/user/user.router'
-import itemRouter from './resources/item/item.router'
-import listRouter from './resources/list/list.router'
 
 export const app = express()
 
@@ -17,13 +13,6 @@ app.use(cors())
 app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
-
-app.post('/signup', signup)
-app.post('/signin', signin)
-
-app.use('/api/user', userRouter)
-app.use('/api/item', itemRouter)
-app.use('/api/list', listRouter)
 
 export const start = async () => {
   try {
