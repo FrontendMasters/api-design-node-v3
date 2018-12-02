30912ff8387d0aa9db6ffea19a489597482092b2
Scott Moss <willscottmoss@gmail.com>
2018-12-01 22:45:47 -0800

    chore

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,19 +1,5 @@
 import { Router } from 'express'
-import controllers from './item.controllers'
 
 const router = Router()
-
-// /api/item
-router
-  .route('/')
-  .get(controllers.getOne)
-  .post(controllers.createOne)
-
-// /api/item/:id
-router
-  .route('/:id')
-  .get(controllers.getOne)
-  .put(controllers.updateOne)
-  .delete(controllers.removeOne)
 
 export default router
