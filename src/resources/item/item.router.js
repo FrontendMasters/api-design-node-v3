731e5eb36e09c1240221860b40a7e24a426b6b4d
Scott Moss <willscottmoss@gmail.com>
2020-04-07 11:32:15 -0700

    fix

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,5 +1,20 @@
 import { Router } from 'express'
-
 const router = Router()
+
+const mockController = (req, res) => {
+  res.json({ message: 'ok' })
+}
+// /api/item
+router
+  .route('/')
+  .get(mockController)
+  .post(mockController)
+
+// /api/item/:id
+router
+  .route('/:id')
+  .get(mockController)
+  .put(mockController)
+  .delete(mockController)
 
 export default router
--- src/server.js
+++ src/server.js
@@ -4,6 +4,9 @@ import morgan from 'morgan'
 import config from './config'
 import cors from 'cors'
 import { connect } from './utils/db'
+import userRouter from './resources/user/user.router'
+import itemRouter from './resources/item/item.router'
+import listRouter from './resources/list/list.router'
 
 export const app = express()
 
@@ -13,6 +16,10 @@ app.use(cors())
 app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
+
+app.use('/api/user', userRouter)
+app.use('/api/item', itemRouter)
+app.use('/api/list', listRouter)
 
 export const start = async () => {
   try {
