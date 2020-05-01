63ba73d93ac558544d8d4974c13fcc853ecd77d6
banudalamanli <banudalamanli@gmail.com>
2020-05-01 12:07:28 +0300

    Add routes

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,5 +1,15 @@
 import { Router } from 'express'
+import controllers from './item.controllers'
 
 const router = Router()
+
+router.route('/me')
+.get(controllers.getItem)
+.post(controllers.createItem)
+
+router.route('/me/:id')
+.get(controllers.getItem)
+.put(controllers.updateItem)
+.delete(controllers.deleteItem)
 
 export default router
