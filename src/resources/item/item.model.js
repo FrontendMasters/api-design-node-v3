a2ad55018fa6a46d9fabf18f45a8178adf260daf
simonojok19 <simonojok19@gmail.com>
2020-07-18 13:40:07 +0300

    added two fields

--- src/resources/item/item.model.js
+++ src/resources/item/item.model.js
@@ -1,4 +1,17 @@
 import mongoose from 'mongoose'
 
-const itemSchema = new mongoose.Schema({}, { timestamps: true })
+const itemSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+        maxLength: 50
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: ['active', 'complete', 'pastdue'],
+        default: 'active'
+    }
+}, { timestamps: true })
 export const Item = mongoose.model('item', itemSchema)
