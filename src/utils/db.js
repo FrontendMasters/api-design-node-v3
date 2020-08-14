446b2da576f54865277b5208fa25840ee33923c9
Boda <6238558+bodazhao@users.noreply.github.com>
2020-08-14 17:06:31 +0100

    extra connect opts to remove deprecation warnings

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,8 +2,10 @@ import mongoose from 'mongoose'
 import options from '../config'
 
 export const connect = (url = options.dbUrl, opts = {}) => {
-  return mongoose.connect(
-    url,
-    { ...opts, useNewUrlParser: true }
-  )
+  return mongoose.connect(url, {
+    ...opts,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
 }
